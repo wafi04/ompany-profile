d6b355c52b0dbb5c2c61e4eac614577491d72e37
wafi04 <wafiwafiwafi90@gmail.com>
2025-03-05 12:07:38 +0700

    edit footer

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -89,7 +89,7 @@ export function Footer(): JSX.Element {
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
-                <span>hhuniverse7@vazzstore.com</span>
+                <span>universe@gmail.com</span>
               </p>
               <div className="flex items-start gap-3">
                 <svg
